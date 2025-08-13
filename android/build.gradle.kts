feab13aadbec643e155d7f4f0f28e848cb74c302
RowanFayez <fayezrowan5@gmail.com>
2025-08-13 23:32:55 +0200

    firebase setup

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,8 @@
+buildscript {
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.3") // Firebase Google Services
+    }
+}
 allprojects {
     repositories {
         google()
