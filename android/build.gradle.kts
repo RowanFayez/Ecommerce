f6639576f6d0a9acc8841dbf46ca52a46f32f011
RowanFayez <fayezrowan5@gmail.com>
2025-08-16 18:00:36 +0200

    setupfirebase android

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,8 @@
+buildscript {
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.3") // Firebase Google Services
+    }
+}
 allprojects {
     repositories {
         google()
@@ -18,4 +23,11 @@ subprojects {
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
+}
+plugins {
+  // ...
+
+  // Add the dependency for the Google services Gradle plugin
+  id("com.google.gms.google-services") version "4.4.3" apply false
+
 }
